283bf6faf29d62f01a5b82fb1442e4af06dc39dc
Scott Mason <smason@oes.com>
2024-01-12 12:55:50 +0000

    added function to network page

--- functions/network.js
+++ functions/network.js
@@ -1,22 +1,30 @@
-export async function onRequest(context) {
-  class ElementHandler {
-    element(element) {
-      const fact = getFact();
-      element.append(`<div>${fact}</div>`, {html: true});
-    }
-  }
+// export default {
 
-  const res = await fetch(request);
-  return new HTMLRewriter().on('section#content', new ElementHandler()).transform(res);
+//   async fetch(request) {
+
+//     class ElementHandler {
+//       element(element) {
+//         const fact = getFact();
+//         element.append(`<div>${fact}</div>`, {html: true});
+//       }
+//     }
+
+//     const res = await fetch(request);
+//     return new HTMLRewriter().on('section#content', new ElementHandler()).transform(res);
 
 
-  async function getFact() {
-    const response = await fetch(`https://catfact.ninja/fact`);
-    try {
-      const result = await response.json();
-      return result.fact;
-    } catch (err) {
-      return err;
-    }
-  }
+//     async function getFact() {
+//       const response = await fetch(`https://catfact.ninja/fact`);
+//       try {
+//         const result = await response.json();
+//         return result.fact;
+//       } catch (err) {
+//         return err;
+//       }
+//     }
+//   }
+// }
+
+export function onRequest(context) {
+  return new Response("Hello, world!")
 }
